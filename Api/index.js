1c453e2ccff5727710dc18d0c31229828fb19281
shreyansh12333 <shreyanpandey30@gmail.com>
2024-06-15 02:25:23 +0530

    create user model

--- Api/index.js
+++ Api/index.js
@@ -1,6 +1,19 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+dotenv.config({ path: "../.env" });
 
 const app = express();
+const connectToMongodb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_SECRET);
+    console.log("Connected to mongodb");
+  } catch (error) {
+    console.log(error);
+  }
+};
+connectToMongodb();
+
 app.listen(3000, () => {
   console.log("server listening on port 3000");
 });
