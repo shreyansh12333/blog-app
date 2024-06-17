bcbf7dc8bf7fb54eae63030f31c3b8d073ba053d
shreyansh12333 <shreyanpandey30@gmail.com>
2024-06-17 09:26:59 +0530

    complete sign-up page functionality

--- Api/controllers/auth.controllers.js
+++ Api/controllers/auth.controllers.js
@@ -1,6 +1,10 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+dotenv.config({ path: "../.env" });
+// console.log(process.env.ACCESS_SECRET_TOKEN);
 export const signup = async (req, res, next) => {
   const { email, userName, password } = req.body;
   if (
@@ -18,6 +22,40 @@ export const signup = async (req, res, next) => {
   try {
     await user.save();
     res.status(201).json("user is created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+export const signin = async (req, res, next) => {
+  const email = req.body.email;
+  const userPassword = req.body.password;
+  // console.log(email);
+  try {
+    if (!email || !userPassword) {
+      return next(errorHandler("All fields are required", 402));
+    }
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+      return next(errorHandler("Invalid credentials", 401));
+    }
+    const verifiedUser = bcryptjs.compareSync(userPassword, validUser.password);
+
+    if (!verifiedUser) {
+      return next(errorHandler("Invalid credentials", 401));
+    }
+    console.log(verifiedUser);
+    const token = jwt.sign(
+      { id: validUser._id },
+      process.env.ACCESS_SECRET_TOKEN
+    );
+
+    const { password, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 360000);
+    res
+      .cookie("access token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,5 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiFillGoogleCircle } from "react-icons/ai";
+import { Link, useNavigate } from "react-router-dom";
+import { Alert, Spinner } from "flowbite-react";
 
 export default function Signin() {
-  return <div>Sign in</div>;
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    const newFormData = { ...formData, [e.target.id]: e.target.value };
+    setFormData(newFormData);
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setErrorMessage(null);
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setErrorMessage(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      navigate("/");
+    } catch (error) {
+      setErrorMessage(error);
+      return;
+    }
+  };
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center">
+        <div className="flex flex-col items-center justify-center mr-7 gap-2">
+          <h1 className="text-3xl font-bold">
+            <span className="text-3xl bg-gradient-to-r from-purple-700 via-purple-500 to-pink-500 rounded-lg p-2 text-white">
+              Shreyansh's{" "}
+            </span>
+            Blog
+          </h1>
+          <p className="text-sm mt-5">
+            This is a demo project. You can sign up with your email and password
+            or with Google.
+          </p>
+        </div>
+        <div className="flex justify-center items-center">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <label for="email" className="font-semibold mb-0">
+              email
+            </label>
+            <input
+              id="email"
+              type="email"
+              placeholder="name@company.com"
+              className="w-96 rounded-lg p-2 bg-slate-100 "
+              onChange={handleChange}
+            ></input>
+            <label for="password" className="font-semibold mb-0">
+              password
+            </label>
+            <input
+              id="password"
+              type="password"
+              className="w-96 rounded-lg p-2 bg-slate-100"
+              onChange={handleChange}
+            ></input>
+            <button
+              className="text-white bg-gradient-to-r from-purple-700 via-purple-500 to-pink-500 p-3 rounded-lg w-96"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "SignUp"
+              )}
+            </button>
+            <button className="border-red-500 p-2 border-2 rounded-lg flex items-center justify-center w-96">
+              <AiFillGoogleCircle className="w-7 h-7 rounded-full" />
+              Continue with google
+            </button>
+            <p>
+              Have an account?{" "}
+              <span className="text-blue-500">
+                <Link>Sign in</Link>
+              </span>
+            </p>
+            {errorMessage && (
+              <Alert className="mt-5" color="failure">
+                {errorMessage}
+              </Alert>
+            )}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
